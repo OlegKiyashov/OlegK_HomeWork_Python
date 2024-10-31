7b34b70199130e293da11a2dbb54f8aadbd1fcd3
OlegKiyashov <olegkiyashov@gmail.com>
2024-10-31 15:55:39 +0700

    Закоммитить все изменения

--- 02_lesson/lesson_2_task_3.py
+++ 02_lesson/lesson_2_task_3.py
@@ -2,9 +2,10 @@
 import math
 
 def square(side):
-# Вычисляем площадь квадрата
+    # Вычисляем площадь квадрата
+
     area = side * side
-    return math.ceil(area) # Округляем вверх
+    return math.ceil(area)  # Округляем вверх
 
 side_length = 5.3
 result = square(side_length)
