a57577326c58df0c9d1147597dfc797c0a7e929e
OlegKiyashov <olegkiyashov@gmail.com>
2024-11-28 22:29:35 +0700

    Изменён третий скрипт для lesson6

--- 06_lesson/src_picture.py
+++ 06_lesson/src_picture.py
@@ -7,17 +7,26 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 
 try:
-    # Шаг 1: Перейти на сайт
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java"
-               "/loading-images.html")
+    # Шаг 1: Перейти на страницу
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-    # Шаг 2: Дождаться появления изображения с id "award"
+    # Шаг 2: Ожидать появления текста "Done!" в элементе с id="text", что указывает на завершение загрузки картинок
     WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.ID, "award"))
+        EC.text_to_be_present_in_element((By.ID, "text"), "Done!")
     )
 
-    # Шаг 3: Вывести значение атрибута src в консоль
-    print(driver.find_element(By.ID, "award").get_dom_attribute("src"))
+    # Шаг 3: Найти все картинки на странице
+    images = driver.find_elements(By.TAG_NAME, "img")
+
+    # Проверка, есть ли хотя бы 3 картинки
+    if len(images) >= 3:
+        # Шаг 4: Получить атрибут src у 3-й картинки
+        third_image_src = images[3].get_attribute("src")
+
+        # Вывести значение атрибута src третьей картинки в консоль
+        print("Значение атрибута 'src' у 3-й картинки:", third_image_src)
+    else:
+        print("На странице меньше 3-х картинок.")
 
 finally:
     # Закрыть браузер
